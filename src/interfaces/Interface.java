e1da873ce1b83ad388d31feb5bf414526af197af
Pedro Bernardes <pedrobernardesfs@gmail.com>
2026-05-26 20:21:54 -0300

    Create Interface.java

---
+++ src/interfaces/Interface.java
@@ -0,0 +1,8 @@
+package interfaces;
+public interface Conceitos {
+
+    String EXCELENCIA = "Parab�ns, voc� atingiu todos os indicadores...";
+    String SATISFATORIO = "Parab�ns, voc� obteve aproveitamento satisfat�rio...";
+    String INSUFICIENTE = "Voc� n�o atingiu o m�nimo esperado para aprova��o";
+    void classificarNota(double nota);
+}
