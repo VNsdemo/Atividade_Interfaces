ad2651d2ebfc4f6ac03e56b3be1378e93497590f
Pedro Bernardes <pedrobernardesfs@gmail.com>
2026-05-26 20:22:34 -0300

    Delete Interface.java

--- Interface.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-public interface Conceitos {
-
-    String EXCELENCIA = "Parabéns, você atingiu todos os indicadores...";
-    String SATISFATORIO = "Parabéns, você obteve aproveitamento satisfatório...";
-    String INSUFICIENTE = "Você não atingiu o mínimo esperado para aprovação";
-    void classificarNota(double nota);
-}
